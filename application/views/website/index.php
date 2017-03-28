04da84af71ef84f63776dcdd67b083932ad069fc
misaelbelocura <misaelbelocura@gmail.com>
2017-03-29 01:58:59 +0800

    mao pani napuno kai ako ge usab ang gahapon nako buhat

--- application/views/website/index.php
+++ application/views/website/index.php
@@ -1,55 +1,97 @@
+	<div class="container" style=" background: #f8f8f8;">	
+		<!--header Navbar-->
 
-		 <!-- Static navbar -->
-    <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Mano Construction & Enterprises</a>
-        </div>
+		<nav class="navbar-default">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="#"><img src="images/website/logo1.png" style="max-width: 275px;"></a>
+		    </div>
 
-		 <form class="navbar-form navbar-left" role="search">
-		 	<h5>PRODUCT SEARCH</h5>
-			  <div class="form-group">
-			    <input type="text" class="form-control" placeholder="Search">
-			  </div>
-		  <div class="btn-group" role="group">
-		    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-		      Dropdown
-		      <span class="caret"></span>
-		    </button>
-		    <ul class="dropdown-menu">
-		      <li><a href="#">Dropdown link</a></li>
-		      <li><a href="#">Dropdown link</a></li>
-		    </ul>
-		  </div>
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      
+		      <form class="navbar-form navbar-right" style="margin-top: 38px;">
+		        <div class="form-group">
+		          <input type="text" class="form-control" placeholder="Search">
+		        </div>
+		        <button type="submit" class="btn btn-default">Submit</button>
+		      </form>
+		      <ul class="nav nav navbar-nav navbar-right" style="margin-top: 32px; font-size: 15px; font-weight: 600;" >
 
-		  <div class="btn-group" role="group">
-		    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-		      Dropdown
-		      <span class="caret"></span>
-		    </button>
-		    <ul class="dropdown-menu">
-		      <li><a href="#">Dropdown link</a></li>
-		      <li><a href="#">Dropdown link</a></li>
-		    </ul>
-		   </div>
-				<button type="submit" class="btn btn-default">Submit</button>
-			</form>
 
-		<ul class="nav nav-pills">
-		  <li role="presentation" class="active"><a href="#">Home</a></li>
-		  <li role="presentation"><a href="#">Profile</a></li>
-		  <li role="presentation"><a href="#">Messages</a></li>
-		</ul>
+		        <li><a href="#">Home</a></li>
+		        <li><a href="#">Contact Us</a></li>
+		        <li><a href="#">About Us</a></li>
+		        <li class="dropdown">
+		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Categories <span class="caret"></span></a>
+		          <ul class="dropdown-menu">
+		            <li><a href="#">Action</a></li>
+		            <li><a href="#">Another action</a></li>
+		            <li><a href="#">Something else here</a></li>
+		          </ul>
+		        </li>
+		        <li class="dropdown">
+		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Brands <span class="caret"></span></a>
+		          <ul class="dropdown-menu">
+		            <li><a href="#">Action</a></li>
+		            <li><a href="#">Another action</a></li>
+		            <li><a href="#">Something else here</a></li>
+		          </ul>
+		        </li>
+
+		      </ul>
+		    </div><!-- /.navbar-ne sa ubos -->
+		    
+		</nav>
+		<!--close for header Navbar-->
+
+		<!--for slider-->
+		<div class="col-sm-12">
+
+		  <div id="my-slider" class="carousel slide" data-ride="carousel">
+
+		    <!-- Indicators -->
+		    <ol class="carousel-indicators">
+		      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		      <li data-target="#myCarousel" data-slide-to="1"></li>
+		    </ol>
+
+		    <!-- Wrapper for slides -->
+		    <div class="carousel-inner" role="listbox">
+		      <div class="item active">
+		        <img src="images/website/slide1.jpg" alt="slide1" width="1500" height="345"/>
+
+		      </div>
+
+		      <div class="item">
+		        <img src="images/website/slide2.jpg" alt="slide2" width="1500" height="345"/>
+		      </div>
+		    </div>
+
+		    <!-- Left and right controls -->
+		    <a class="left carousel-control" href="#my-slider" role="button" data-slide="prev">
+		      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		      <span class="sr-only">Previous</span>
+		    </a>
+		    <a class="right carousel-control" href="#my-slider" role="button" data-slide="next">
+		      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		      <span class="sr-only">Next</span>
+		    </a>
 		  </div>
 		</div>
+		</div>
+		<!--END for Slider-->
+	
+
+	
+	</div><!-- /.container-fluid -->
 
 
 
-       
 
