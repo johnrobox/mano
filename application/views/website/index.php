4f3b5e7416f262918586e4d7efc7f5d1fb5eabb1
misaelbelocura <misaelbelocura@gmail.com>
2017-03-29 18:38:05 +0800

    mego naa ko ge puno. paki check lang ko mego

--- application/views/website/index.php
+++ application/views/website/index.php
@@ -10,7 +10,7 @@
 		        <span class="icon-bar"></span>
 		        <span class="icon-bar"></span>
 		      </button>
-		      <a class="navbar-brand" href="#"><img src="images/website/logo1.png" style="max-width: 275px;"></a>
+		      <a class="navbar-brand" href="http://localhost/mano"><img src="images/website/logo1.png" style="max-width: 275px;"></a>
 		    </div>
 
 		    <!-- Collect the nav links, forms, and other content for toggling -->
@@ -25,9 +25,11 @@
 		      <ul class="nav nav navbar-nav navbar-right" style="margin-top: 32px; font-size: 15px; font-weight: 600;" >
 
 
-		        <li><a href="#">Home</a></li>
-		        <li><a href="#">Contact Us</a></li>
+		        <li><a href="http://localhost/mano">Home</a></li>
+		        <li><a href="#">Store Location</a></li>
+		        <li><a href="http://localhost/mano/index.php/HomepageController/contactUs">Contact Us</a></li>
 		        <li><a href="#">About Us</a></li>
+
 		        <li class="dropdown">
 		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Categories <span class="caret"></span></a>
 		          <ul class="dropdown-menu">
@@ -36,23 +38,14 @@
 		            <li><a href="#">Something else here</a></li>
 		          </ul>
 		        </li>
-		        <li class="dropdown">
-		          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Brands <span class="caret"></span></a>
-		          <ul class="dropdown-menu">
-		            <li><a href="#">Action</a></li>
-		            <li><a href="#">Another action</a></li>
-		            <li><a href="#">Something else here</a></li>
-		          </ul>
-		        </li>
-
-		      </ul>
+		       </ul>
 		    </div><!-- /.navbar-ne sa ubos -->
 		    
 		</nav>
 		<!--close for header Navbar-->
 
 		<!--for slider-->
-		<div class="col-sm-12">
+		
 
 		  <div id="my-slider" class="carousel slide" data-ride="carousel">
 
@@ -84,12 +77,256 @@
 		      <span class="sr-only">Next</span>
 		    </a>
 		  </div>
-		</div>
-		</div>
 		<!--END for Slider-->
-	
+		<!--Container-->
+		<h2 style="text-align: center; font-size: 43px;">Products</h2>
 
+  			<div class="row">
+			<div class="col-sm-3" style="background: url(images/website/catbox-paints-and-sundries.jpg) 10px 0 no-repeat #fff; height: 300px;     margin: 0px;">
+				<h3 style="text-align: center;">Paints</h3>
+
+				<ul style="list-style: none; padding: 0;">
+				  <li>
+				  	<a href="#">Paints</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Brushes & Rollers</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Caulks & Sealants</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Adhesives & Tapes</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Ladders</a>
+				  </li>	
+
+				   <span>
+				  	<a style="color: red;" href="#">VIEW ALL ></a>
+				  </span>
+
+				</ul>
+
+			</div>
+
+			<div class="col-sm-3"style="background: url(images/website/catbox-home-hardware.jpg) 10px 0 no-repeat #fff; height: 300px;     margin: 0px;">
+				<h3 style="text-align: center;">Home Hardware</h3>
+
+				<ul style="list-style: none; padding: 0;">
+				  <li>
+				  	<a href="#">Building Materials & Supplies</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Door & Cabinet Hardware</a>
+				  </li>
+				  <li>
+				  	<a href="#">Industrial & Exhaust Fans</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Locksets & Padlocks</a>
+				  </li>
+				  <li>
+				  	<a href="#">Bathroom Accessories</a>
+				  </li>
+				  <li>
+				  	<a href="#">Drawers & Closets</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Mats & Carpets</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Trash Bins</a>
+				  </li>
+				  <li>
+				  	<a href="#">Pails & Buckets</a>
+				  </li>
+				  <li>
+				  	<a href="#">Dish Drainers & Kitchen Accessories</a>
+				  </li>
+				  <li>
+				  	<a href="#">Cleaning Implements</a>
+				  </li>		
+				  <li>
+				  	<a href="#">Stackables & Storage Accessories</a>
+				  </li>	
+
+				   <span>
+				  	<a style="color: red;" href="#">VIEW ALL ></a>
+				  </span>	
+				</ul>
+				
+			</div>
+
+			<div class="col-sm-3" style="background: url(images/website/catbox-plumbing.jpg) 10px 0 no-repeat #fff; height: 300px;  margin: 0px;">
+			<h3 style="text-align: center;">Plumbing</h3>
+				<ul style="list-style: none; padding: 0;">
+				  <li>
+				  	<a href="#">Air Purifier</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Faucets</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Fittings</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Showers & Bidets</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Water Filtration</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Water Heaters</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Water Storage & Pumps</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Watercloset & Accessories</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Sink, Lavatory & Accessories</a>
+				  </li>	
+				
+				   <span>
+				  	<a style="color: red;" href="#">VIEW ALL ></a>
+				  </span>
+				</ul>
+				
+			</div>
+
+			<div class="col-sm-3" style="background: url(images/website/catbox-electrical.jpg) 10px 0 no-repeat #fff; height: 300px;     margin: 0px;">
+			<h3 style="text-align: center;">Electrical</h3>
+				<ul style="list-style: none; padding: 0;">
+				  <li>
+				  	<a href="#">Bulbs & Fluorescent Lights</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Lighting Fixtures</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Flashlights & Batteries</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Rechargeables</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Power Supply</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Extension Cords, Wires & Cables</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Wiring Devices</a>
+				  </li>	
+				  <li>
+				  	<a href="#">Audio, Video & Telephone Supplies</a>
+				  </li>	
+				
+				   <span>
+				  	<a style="color: red;" href="#">VIEW ALL ></a>
+				  </span>
+				</ul>
+				
+			</div>
+
+		</div>
+
+<br>
+		<div class="row">
+			    <div class="col-sm-3 col-md-4" style="background-color: #ffffff;" >
+			    <h3 style="text-align: center;">Welcome To</h3>
+			    	<h2 style="text-align: center;">Mano Construction & Enterprises</h2>
+			    	<br>
+			    	<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+			    
+					</div>
+			    <div class="col-sm-9 col-md-8">
+			      <div id="my-slider1" class="carousel slide" data-ride="carousel">
+
+					    <!-- Indicators -->
+					    <ol class="carousel-indicators">
+					      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+					      <li data-target="#myCarousel" data-slide-to="1"></li>
+					      <li data-target="#myCarousel" data-slide-to="2"></li>
+					    </ol>
+
+					    <!-- Wrapper for slides -->
+					    <div class="carousel-inner" role="listbox">
+					      <div class="item active">
+					        <img src="images/website/paints1.jpg" alt="slide1" width="1500" height="145"/>
+
+					      </div>
+
+					      <div class="item">
+					        <img src="images/website/househardware.jpg" alt="slide2" width="1500" height="145"/>
+					      </div>
+
+					      <div class="item">
+					        <img src="images/website/electrical.png" alt="slide2" width="1500" height="145"/>
+					      </div>
+					    </div>
+
+					    <!-- Left and right controls -->
+					    <a class="left carousel-control" href="#my-slider1" role="button" data-slide="prev">
+					      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+					      <span class="sr-only">Previous</span>
+					    </a>
+					    <a class="right carousel-control" href="#my-slider1" role="button" data-slide="next">
+					      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+					      <span class="sr-only">Next</span>
+					    </a>
+					  </div>
+			    </div>
+  			</div>
+
+
+		<div class="container">
+				<h2 style="font-size: 43px;">More Business</h2 >
+
+			<div class="row">
+
+				<div class="col-sm-4">
+				<h3 style="text-align: center;">Construction & Enterprises</h3>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>
+	     			 Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+
+				</div>
+
+				<div class="col-sm-4">
+				<h3 style="text-align: center;">Hardware Store</h3>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>
+	      			Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					
+				</div>
+
+				<div class="col-sm-4">
+				<h3 style="text-align: center;">Sari-Sari Store</h3>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>
+	      			Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					
+				</div>
+
+			</div>
+
+
+		</div>
+
+
+				
+
+		
+
+
+		<!--Close Container-->
+
+
+	<br>
 	
+	<br>
+
 	</div><!-- /.container-fluid -->
 
 
