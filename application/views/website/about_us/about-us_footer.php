be6c1612d6305bdfb5c271f4eb29ccb14b8705d3
misaelbelocura <misaelbelocura@gmail.com>
2017-03-31 03:38:26 +0800

    mego naa ko ge add sa footer  3:38 AM

--- application/views/website/about_us/about-us_footer.php
+++ application/views/website/about_us/about-us_footer.php
@@ -17,9 +17,62 @@
 				</ol>
 
 			</div>
-				<div class="container">
+			<div class="container">
 					<h5>© 2017 Mano's Construction & Enterprises , All rights reserved @ Developed by: RiseApp Solutions</h5>
 				</div>
+
+				<section class="box-position">
+				        <div class="col-xs-12 col-md-3 col-sm-4 pull-right">
+
+				             <!-- Message box title    --> 
+				            <div class="panel">
+				               
+				                <div class="panel-heading top-bar" role="tab" id="open-message">
+				                    
+				                    <div class="col-md-10 col-xs-10">
+				                        <p class="panel-title"><span class="glyphicon glyphicon-envelope"></span>   Leave a Message</p>
+				                    </div>
+				                            
+				                    <div class="col-md-2 col-xs-2">
+				                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#message" aria-expanded="false" aria-controls="message">
+				                        <span class="glyphicon  glyphicon-plus"></span></a>
+				                    </div>
+				                            
+				                </div>
+				            </div>
+				                
+				             <!-- Message body	--> 
+				             
+				                <div id="message" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+				                    <div class="panel-body">
+				                        <form  action="#">
+				                        
+				                            <div class="form-group ">
+				                              <input type="text" class="form-control" id="name" placeholder="Name" required="required">
+				                            </div>
+				                            <div class="form-group">
+				                              <input type="text" class="form-control" id="subject" placeholder="Subject" required="required">
+				                            </div>
+				                            <div class="form-group">
+				                              <input type="email" class="form-control" id="email" placeholder="Email" required="required">
+				                            </div>
+				                            <div class="form-group">
+				                              <textarea type="text" class="form-control" id="message" placeholder="Message..." required="required"></textarea>
+				                            </div>
+				                            
+				                             <div class="form-group">
+				                              <input type="submit" class="form-control btn-success" id="submit" value="Send">
+				                            </div>
+				                            
+				                        </form>
+				                    </div>
+				                </div>
+				                
+				        </div>
+
+				</section>
+				
 		</div>
 		<!--End footer-->
+
 </html>
