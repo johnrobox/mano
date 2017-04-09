df2f6071804b1e1718f86f0677113148f36cc11b
misaelbelocura <misaelbelocura@gmail.com>
2017-04-09 19:32:26 +0800

    mego naa ko ge add view all and modal

---
+++ application/views/website/Plumbing/plumbing_footer.php
@@ -0,0 +1,78 @@
+		
+		
+		</body>
+					<!--footer-->
+					<div class="footer">
+						<div class="container">
+						
+							<ol class="breadcrumb" style="font-weight: 600; background-color: #ffffff;">
+							  <li><a href="http://localhost/mano/">Home</a></li>
+							  <li><a href="#">Brands</a></li>
+							  <li><a href="#">Sales & Specials</a></li>
+							  <li><a href="#">Events</a></li>
+							  <li><a href="#">Branches</a></li>
+							  <li><a href="http://localhost/mano/index.php/HomepageController/aboutUs">About Us</a></li>
+							  <li><a href="http://localhost/mano/index.php/HomepageController/contactUs">Contact Us</a></li>
+							  <li><a href="#">Careers</a></li>
+							  <li><a href="#">Privacy Policy</a></li>
+							  <li><a href="#">Disclaimer</a></li>
+							</ol>
+
+						</div>
+						<div class="container">
+								<h5>© 2017 Mano's Construction & Enterprises , All rights reserved @ Developed by: RiseApp Solutions</h5>
+							</div>
+					</div>
+							<section class="box-position">
+							        <div class="col-xs-12 col-md-3 col-sm-4 pull-right">
+
+							             <!-- Message box title    --> 
+							            <div class="panel">
+							               
+							                <div class="panel-heading top-bar" role="tab" id="open-message">
+							                    
+							                    <div class="col-md-10 col-xs-10">
+							                        <p class="panel-title"><span class="glyphicon glyphicon-envelope"></span>   Leave a Message</p>
+							                    </div>
+							                            
+							                    <div class="col-md-2 col-xs-2">
+							                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#message" aria-expanded="false" aria-controls="message">
+							                        <span class="glyphicon  glyphicon-plus"></span></a>
+							                    </div>
+							                            
+							                </div>
+							            </div>
+							                
+							             <!-- Message body	--> 
+							             
+							                <div id="message" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+							                    <div class="panel-body">
+							                        <form  action="#">
+							                        
+							                            <div class="form-group ">
+							                              <input type="text" class="form-control" id="name" placeholder="Name" required="required">
+							                            </div>
+							                            <div class="form-group">
+							                              <input type="text" class="form-control" id="subject" placeholder="Subject" required="required">
+							                            </div>
+							                            <div class="form-group">
+							                              <input type="email" class="form-control" id="email" placeholder="Email" required="required">
+							                            </div>
+							                            <div class="form-group">
+							                              <textarea type="text" class="form-control" id="message" placeholder="Message..." required="required"></textarea>
+							                            </div>
+							                            
+							                             <div class="form-group">
+							                              <input type="submit" class="form-control btn-success" id="submit" value="Send">
+							                            </div>
+							                            
+							                        </form>
+							                    </div>
+							                </div>
+							                
+							        </div>
+
+							</section>
+
+</html>
+<!--End footer-->
